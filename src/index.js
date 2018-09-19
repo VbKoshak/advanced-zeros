9c00346118417b478cbce8591f6ac5354e58148e
Vlad_Boltrukanis <boltrukanisvlad@gmail.com>
2018-09-19 21:06:59 +0300

    Need Some Optimization

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+ var min = number;
+ var st, base1 = base;
+ var count;
+ var number1;
+ for (var i = 2; i <= base; i++)
+ {
+     if (base%i == 0)
+     {
+         st = 0;
+         while(base1%i == 0)
+         {
+             st++;
+             base1 /= i;
+         }
+         count = 0;
+         number1 = number;
+         for (var nus = i; nus <= number; nus+=i)
+         {
+             var tnus = nus;
+             while(tnus%i==0)
+             {
+                 count = count +1;
+                 tnus/=i;
+             }
+         }
+         if (min > count/st)
+         {
+            min = count/st;
+        };
+     }
+ }
+ return Math.trunc(min);
 }
